2bcd7dcecb3dc7385eeb91f34b39e129864d4efe
Reetik Rajan <r4reetik@users.noreply.github.com>
2024-03-12 02:47:01 +0530

    Refactor SEED contract documentation

--- contracts/SEED.sol
+++ contracts/SEED.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @title SEED
-/// @author Garden Finance
-/// @notice SEED is the native token of Garden Finance and is used for governance and staking.
-/// @dev SEED is an ERC20 token with a fixed supply of 147,000,000 and 18 decimals.
+/**
+ * @title   SEED
+ * @author  Garden Finance
+ * @notice  SEED is the native token of Garden Finance and is used for governance and staking.
+ * @dev     SEED is an ERC20 token with a fixed supply of 147,000,000 and 18 decimals.
+ *          The token is minted to the deployer of the contract.
+ */
 contract SEED is ERC20 {
     constructor() ERC20("SEED", "SEED") {
         _mint(_msgSender(), 147_000_000 * (10 ** decimals()));
