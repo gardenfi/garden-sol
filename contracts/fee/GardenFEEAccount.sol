f14db704938c0745fd065e45328c242be7caabdc
yash1io <yashwanth@renlabs.org>
2024-03-22 00:43:08 +0530

    fix secret management

--- contracts/fee/GardenFEEAccount.sol
+++ contracts/fee/GardenFEEAccount.sol
@@ -54,9 +54,9 @@ contract GardenFEEAccount is EIP712Upgradeable {
     uint256 public amount;
     uint256 public nonce;
     uint256 public expiration;
-    uint256 public secretsProvided;
 
-    mapping(bytes => bool) public secretsClaimed;
+    mapping(bytes => uint256) public secretsClaimed;
+    mapping(bytes32 => bytes) public secrets;
 
     uint256 private constant TWO_DAYS = 2 * 7200;
 
@@ -115,7 +115,7 @@ contract GardenFEEAccount is EIP712Upgradeable {
      * @param amount_ The amount of tokens to be claimed.
      * @param nonce_ The nonce value for the claim message.
      * @param htlcs The array of HTLCs in the claim.
-     * @param secrets The array of secrets corresponding to the HTLCs.
+     * @param secrets_ The array of secrets corresponding to the HTLCs.
      * @param funderSig The signature of the funder for the claim message.
      * @param recipientSig The signature of the recipient for the claim message.
      */
@@ -123,38 +123,43 @@ contract GardenFEEAccount is EIP712Upgradeable {
         uint256 amount_,
         uint256 nonce_,
         HTLC[] memory htlcs,
-        bytes[] memory secrets,
+        bytes[] memory secrets_,
         bytes memory funderSig,
         bytes memory recipientSig
     ) external {
-        require(htlcs.length == secrets.length, "FeeAccount: invalid input");
+        require(htlcs.length == secrets_.length, "FeeAccount: invalid input");
+        require(!(htlcs.length > 0 && nonce_ == 0), "FeeAccount: zero nonce claim cannot contain htlcs");
         bytes32 claimID = claimHash(amount_, nonce_, htlcs);
 
         if (nonce == nonce_ && expiration != 0) {
             amount_ = amount;
         }
 
-        uint256 localSecretsProvided = 0;
+        bool secretsProcessed = false;
+
         for (uint256 i = 0; i < htlcs.length; i++) {
-            if (!(secretsClaimed[secrets[i]] && nonce == nonce_)) {
-                if (htlcs[i].timeLock > block.number && sha256(secrets[i]) == htlcs[i].secretHash) {
-                    localSecretsProvided++;
-                    secretsClaimed[secrets[i]] = true;
+            if (secretsClaimed[secrets[htlcs[i].secretHash]] > 0) {
+                if (secretsClaimed[secrets[htlcs[i].secretHash]] != nonce_) {
+                    secretsProcessed = true;
+                    secretsClaimed[secrets_[i]] = nonce_;
                     amount_ += htlcs[i].sendAmount;
                     amount_ -= htlcs[i].recieveAmount;
                 }
-            } else {
-                localSecretsProvided++;
+                continue;
+            }
+            if (htlcs[i].timeLock > block.number && sha256(secrets_[i]) == htlcs[i].secretHash) {
+                secretsProcessed = true;
+                secretsClaimed[secrets_[i]] = nonce_;
+                secrets[htlcs[i].secretHash] = secrets_[i];
+                amount_ += htlcs[i].sendAmount;
+                amount_ -= htlcs[i].recieveAmount;
             }
         }
 
         require(amount_ <= totalAmount(), "FeeAccount: invalid amount");
         if (expiration != 0) {
             // a claim exists, so should satisfy override conditions
-            require(
-                nonce_ > nonce || (nonce_ == nonce && localSecretsProvided > secretsProvided),
-                "FeeAccount: override conditions not met"
-            );
+            require(nonce_ > nonce || (nonce_ == nonce && secretsProcessed), "FeeAccount: override conditions not met");
         }
 
         // verify funder and recipient signatures
@@ -164,7 +169,6 @@ contract GardenFEEAccount is EIP712Upgradeable {
         require(recipientSigner == recipient, "FeeAccount: invalid recipient signature");
 
         // update global claim state
-        secretsProvided = localSecretsProvided;
         expiration = block.number + TWO_DAYS;
         amount = amount_;
         nonce = nonce_;
